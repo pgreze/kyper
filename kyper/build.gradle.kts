18130377246b93cc9cdbe938ae9abb563329a128
pgreze <pierrick.greze@gmail.com>
2023-01-01 13:46:40 +0900

    Add TypesKtTest

--- kyper/build.gradle.kts
+++ kyper/build.gradle.kts
@@ -20,8 +20,11 @@ dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    // https://kotest.io/docs/assertions/assertions.html
+    testImplementation("io.kotest:kotest-assertions-core:4.0.7")
 }
 
 publishing {
---
+++ kyper/src/test/kotlin/kyper/TypesKtTest.kt
@@ -0,0 +1,69 @@
+package kyper
+
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.io.File
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.nio.file.Path
+import java.util.stream.Stream
+import kotlin.reflect.KType
+import kotlin.reflect.full.declaredMemberProperties
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.javaGetter
+import kotlin.reflect.typeOf
+
+class TypesKtTest {
+
+    enum class Choice { OK, NO }
+
+    data class MyClass(
+        val string: String = "arg",
+        val int: Int = 1,
+        val float: Float = 1.2f,
+        val double: Double = 3.14,
+        val long: Long = Long.MAX_VALUE,
+        val boolean: Boolean = true,
+        val bigInteger: BigInteger = BigInteger.valueOf(12),
+        val bigDecimal: BigDecimal = BigDecimal.valueOf(12.3),
+        val file: File = File("file"),
+        val path: Path = Path.of("path"),
+        val choice: Choice = Choice.NO,
+    )
+
+    companion object {
+        private val kFunction = MyClass::class.primaryConstructor!!
+        private val instance = kFunction.callBy(mapOf())
+
+        @JvmStatic
+        private fun typeToValueProvider(): Stream<Arguments> =
+            MyClass::class.declaredMemberProperties
+                .also { require(it.size == 11) { "Invalid resolution of properties=$it" } }
+                .map { Arguments.of(it.returnType, it.javaGetter!!.invoke(instance)) }
+                .stream()
+    }
+
+    @ParameterizedTest
+    @MethodSource("typeToValueProvider")
+    fun convert(kType: KType, value: Any) {
+        kType.convert(value.toString()) shouldBe value
+    }
+
+    @Test
+    fun `convert unsupported type`() {
+        val kType = typeOf<StringBuilder>()
+
+        assertThrows<RuntimeException> { kType.convert("") }
+            .message shouldContain "$kType"
+    }
+
+    @Test
+    fun enumValues() {
+        typeOf<Choice>().enumValues() shouldBe Choice.values()
+    }
+}
