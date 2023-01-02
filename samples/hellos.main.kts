2f70b2287a25818114f03ad58dc77caa0f5aa54e
pgreze <pierrick.greze@gmail.com>
2023-01-02 13:12:55 +0900

    Update samples to use the latest version

--- samples/hellos.main.kts
+++ samples/hellos.main.kts
@@ -1,6 +1,8 @@
 #!/usr/bin/env kotlinc -script
 
-@file:DependsOn("/Users/pgreze/git/pgreze/kyper/build/libs/kyper.jar")
+@file:Repository("https://repo.maven.apache.org/maven2/")
+@file:DependsOn("com.github.pgreze:kyper:0.2")
+// @file:DependsOn("/Users/pgreze/git/pgreze/kyper/build/libs/kyper.jar")
 
 import com.github.pgreze.kyper.Help
 import com.github.pgreze.kyper.kyper
